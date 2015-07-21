899f423d081894f3fa472e822e35e3367e2fe593
souparno <souparno.majumder@gmail.com>
2015-07-21 22:07:11 +0530

    initial commit

---
+++ TestRunner.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.notification.RunListener;
+
+
+import org.junit.runner.Description;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+class TestRunner {
+
+  public static void main (String args[]) {
+    
+   List<Class<?>> classes= new ArrayList<Class<?>>();
+    
+    for(String each : args){
+      try {
+        classes.add(Class.forName(each));
+      } catch (ClassNotFoundException e) {
+        System.out.println("Class Not found");
+      }
+    }
+
+    JUnitCore core = new JUnitCore();
+    RunListener listener = new XmlListener();
+    core.addListener(listener);
+    core.run(classes.toArray(new Class[0]));   
+  }
+}
+
+
+
+class XmlListener extends RunListener {
+
+    public void testRunStarted(Description description) throws Exception {
+        System.out.println("Number of tests to execute: " + description.testCount());
+    }
+
+    public void testRunFinished(Result result) throws Exception {
+        System.out.println("Number of tests executed: " + result.getRunCount());
+    }
+
+    public void testStarted(Description description) throws Exception {
+        System.out.println("Starting: " + description.getMethodName());
+    }
+
+    public void testFinished(Description description) throws Exception {
+        System.out.println("Finished: " + description.getMethodName());
+    }
+
+    public void testFailure(Failure failure) throws Exception {
+        System.out.println("Failed: " + failure.getDescription().getMethodName());
+    }
+
+    public void testAssumptionFailure(Failure failure) {
+        System.out.println("Failed: " + failure.getDescription().getMethodName());
+    }
+
+    public void testIgnored(Description description) throws Exception {
+        System.out.println("Ignored: " + description.getMethodName());
+    }
+
+}
